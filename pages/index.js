137d612e28f2c6e8daaf4dfd013463ef70dba3aa
Joshua Melo <joshuamelo@gmail.com>
2021-09-28 23:31:42 -0400

    added the title and paragraph to home screen

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,27 @@ export default function Home() {
       </Head>
       <Nav />
       <div className="w-screen h-screen flex justify-end items-center">
-        <div className="w-4/12 flex mr-20">
-          <div className="flex flex-col m-2 w-1/2 justify-center items-center">
-            <span>Been here before?</span>
-            <Link href="/quiz" passHref>
-              <a className="bg-green-300 p-2 rounded-lg w-28 text-center">Start Quiz</a>
-            </Link>
-          </div>
-          <div className="flex flex-col m-2 w-1/2 justify-center items-center">
-            <span>First time here?</span>
-            <Link href="/guided-tour" passHref>
-              <a className="bg-green-300 p-2 rounded-lg w-28 text-center">Take A Tour</a>
-            </Link>
+        <div className="w-4/12 flex mr-20 flex-col">
+          <span className="flex text-center justify-center text-4xl mb-2 text-green-400">
+            Welcome to Meal.Match
+          </span>
+          <span className="text-center mb-8">
+            Have items in your pantry but not sure what to make? Fill out our short quiz to see
+            recipies that match what you have, not what you need.
+          </span>
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="flex flex-col m-2 w-1/2 justify-center items-center">
+              <span>Been here before?</span>
+              <Link href="/quiz" passHref>
+                <a className="bg-green-300 p-2 rounded-lg w-28 text-center">Start Quiz</a>
+              </Link>
+            </div>
+            <div className="flex flex-col m-2 w-1/2 justify-center items-center">
+              <span>First time here?</span>
+              <Link href="/guided-tour" passHref>
+                <a className="bg-green-300 p-2 rounded-lg w-28 text-center">Take A Tour</a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
