95f4965b95e6dd39791dc7cc773ca25a2241c122
Josh Melo <joshmelo@Joshs-MacBook-Pro.local>
2022-03-30 01:03:34 -0400

    not styling, added mockdata

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,14 @@
-export async function getRecipes(query) {
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.NEXT_PUBLIC_APP_ID}&app_key=${process.env.NEXT_PUBLIC_APP_KEY}&random=true&field=url&field=label&field=image&imageSize=LARGE&app_id`
+import sampleData from '../sampleData.json'
 
+export async function getRecipes(query) {
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEV') {
+    return { hits: sampleData }
+  }
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.NEXT_PUBLIC_APP_ID}&app_key=${process.env.NEXT_PUBLIC_APP_KEY}&random=true&field=url&field=label&field=image&imageSize=LARGE&app_id`
   try {
     const data = await fetch(url)
     const json = await data.json()
+    console.log(json)
     return json
   } catch (err) {
     console.error(err)
