07bf5c6ed783e1af899077d48bc9f5da35b47321
Josh Melo <joshmelo@Joshs-MacBook-Pro-2.local>
2022-05-29 17:55:16 -0400

    moving query to backend

---
+++ pages/api/getRecipes.ts
@@ -0,0 +1,37 @@
+import sampleData from '../../utils/data.json'
+
+export default async function handler(req, res) {
+  const { foodQuery } = req
+
+  if (process.env.ENVIRONMENT === 'DEV' && process.env.USE_MOCK_DATA === 'TRUE') {
+    return { hits: sampleData }
+  }
+
+  const queryParams = [
+    'random=true',
+    'field=url',
+    'field=label',
+    'field=image',
+    'imageSize=LARGE',
+    'field=ingredientLines',
+    'field=calories',
+    'field=totalTime',
+  ].join('&')
+
+  //Recipes API: https://developer.edamam.com/edamam-docs-recipe-api#/
+  const url = [
+    'https://api.edamam.com/api/recipes/v2?type=public',
+    `app_id=${process.env.APP_ID}`,
+    `app_key=${process.env.APP_KEY}`,
+    `q=${foodQuery}`,
+    `${queryParams}`,
+  ].join('&')
+
+  try {
+    const data = await fetch(url)
+    const json = await data.json()
+    res.send(json)
+  } catch (err) {
+    console.error(err)
+  }
+}
--- pages/results.tsx
+++ pages/results.tsx
@@ -5,8 +5,6 @@ import React, { FC, useEffect, useState } from 'react'
 import Nav from '../components/Nav'
 import RecipeCard from '../components/RecipeCard'
 
-import { getRecipes } from '../utils/helpers'
-
 const Results: FC = () => {
   const router = useRouter()
   const { foodQuery } = router.query
@@ -15,9 +13,9 @@ const Results: FC = () => {
   useEffect(() => {
     const retrieveRecipes = async () => {
       try {
-        // await fetch('/api/sendTwilioMessage')
-        const { hits } = await getRecipes(foodQuery.toString())
-        setRecipes(hits.slice(0, 6))
+        const recipies = await fetch('/api/getRecipes', { foodQuery })
+        const { hits } = await recipies.json()
+        setRecipes(await hits.slice(0, 6))
       } catch (error) {
         console.error(error)
       }
