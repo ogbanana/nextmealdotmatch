16718fdcb17ac4516b81becc22b96dd699a5991c
Josh Melo <joshmelo@Joshs-MacBook-Pro-2.local>
2022-05-31 01:38:20 -0400

    warning

--- pages/selectedRecipe.tsx
+++ pages/selectedRecipe.tsx
@@ -73,6 +73,7 @@ const SelectedRecipe: FC = () => {
       }
     })
     setingredientsRenderData(ingredientsRenderObject)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [recipe])
 
   const handleTwilioClick = (event: MouseEvent<HTMLDivElement>) => {
